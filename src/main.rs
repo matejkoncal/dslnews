91275c3438876a1a7e1d965b467c688da03e25eb
Matej Koncal <matej.koncal@resco.net>
2022-03-11 14:29:10 +0100

    Open selected article in browser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-extern crate reqwest;
-
+use inquire::Select;
+use reqwest::get;
 use select::document::Document;
 use select::predicate::{Attr, Name};
 
@@ -10,16 +10,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn load_dsl_news(url: &str) {
-    let resp = reqwest::get(url).await.unwrap();
+    let resp = get(url).await.unwrap();
     assert!(resp.status().is_success());
 
     let document = Document::from_read(resp.text().await.unwrap().as_bytes()).unwrap();
     let news_box = document.find(Attr("id", "news_box")).next().unwrap();
-    for new_item in news_box
+
+    let titles: Vec<String> = news_box
         .find(Name("a"))
         .filter(|x| x.text().chars().next().unwrap() != '(')
-    {
-        let text = new_item.text();
-        println!("{}", text);
-    }
+        .map(|x| x.text())
+        .collect();
+
+    let urls: Vec<String> = news_box
+        .find(Name("a"))
+        .filter(|x| x.text().chars().next().unwrap() != '(')
+        .map(|x| x.attr("href").unwrap().to_string())
+        .collect();
+
+    let ans = Select::new(
+        "Which news do you want to open in the browser?",
+        titles.clone(),
+    )
+    .prompt()
+    .unwrap();
+
+    let index_of_selected = titles.iter().position(|x| x == &ans).unwrap();
+    open::that("http://dsl.sk/".to_string() + &urls[index_of_selected]).unwrap();
 }
