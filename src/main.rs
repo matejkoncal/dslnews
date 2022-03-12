806b347e8083271b7746fb0c413d0b3a8d561aaa
Matej Koncal <matej.koncal@resco.net>
2022-03-12 20:28:48 +0100

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ use inquire::Select;
 use reqwest::get;
 use select::document::Document;
 use select::predicate::{Attr, Name};
+use std::fmt;
+
+struct NewsItem {
+    title: String,
+    link: String,
+}
+
+impl fmt::Display for NewsItem {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.title)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,25 +28,18 @@ async fn load_dsl_news(url: &str) {
     let document = Document::from_read(resp.text().await.unwrap().as_bytes()).unwrap();
     let news_box = document.find(Attr("id", "news_box")).next().unwrap();
 
-    let titles: Vec<String> = news_box
+    let news_items: Vec<NewsItem> = news_box
         .find(Name("a"))
         .filter(|x| x.text().chars().next().unwrap() != '(')
-        .map(|x| x.text())
+        .map(|x| NewsItem {
+            title: x.text().to_string(),
+            link: x.attr("href").unwrap().to_string(),
+        })
         .collect();
 
-    let urls: Vec<String> = news_box
-        .find(Name("a"))
-        .filter(|x| x.text().chars().next().unwrap() != '(')
-        .map(|x| x.attr("href").unwrap().to_string())
-        .collect();
+    let ans = Select::new("Which news do you want to open in the browser?", news_items)
+        .prompt()
+        .unwrap();
 
-    let ans = Select::new(
-        "Which news do you want to open in the browser?",
-        titles.clone(),
-    )
-    .prompt()
-    .unwrap();
-
-    let index_of_selected = titles.iter().position(|x| x == &ans).unwrap();
-    open::that("http://dsl.sk/".to_string() + &urls[index_of_selected]).unwrap();
+    open::that("http://dsl.sk/".to_string() + &ans.link).unwrap();
 }
